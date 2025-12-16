40bf6fe7a51355a793f308cf898b2f681389c8a2
Mik17-sr <miguelangelsierralarrota@hotmail.com>
2025-12-16 13:16:54 -0500

    BD y ventanas

--- src/main/java/com/mycompany/controller/VentaController.java
+++ src/main/java/com/mycompany/controller/VentaController.java
@@ -1,9 +1,23 @@
 package com.mycompany.controller;
 
-/**
- *
- * @author Miguel
- */
+import com.mycompany.vista.FrmPrincipal;
+
 public class VentaController {
+    private FrmPrincipal frm;
+
+    public VentaController() {
+        super();
+    }
     
+    public VentaController(FrmPrincipal frm) {
+        this.frm = frm;
+    }
+
+    public FrmPrincipal getFrm() {
+        return frm;
+    }
+
+    public void setFrm(FrmPrincipal frm) {
+        this.frm = frm;
+    }
 }
